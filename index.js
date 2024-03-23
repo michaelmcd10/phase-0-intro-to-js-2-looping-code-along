feabb13dcf77d2e39d8b2dbe99ada365a614d079
michael <michaelmcd10@gmail.com>
2024-03-23 11:44:17 +0300

    looping

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(num) {
+    if (typeof num !== 'number' || num <= 0 || !Number.isInteger(num)) {
+        console.log("Please provide a positive integer.");
+        return;
+    }
+    
+    for (let i = num; i >= 0; i--) {
+        console.log(i);
+    }
+}
+
+writeCards(10);
+
